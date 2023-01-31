ce2047a000c726ad68a2cd71336e686e34b9ce87
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2023-01-31 10:56:55 +0100

    added to the prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -30,7 +30,8 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
-    });
+      max_tokens: 50,
+    });    
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
